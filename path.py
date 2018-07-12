06f0cc8a19c86e9d5b881d4850376349e0667ede
ama-afrakoma <richardlawer@gmail.com>
2018-07-12 22:29:11 +0000

    updating files

--- path.py
+++ path.py
@@ -1,13 +1,14 @@
 from flask import Flask ,render_template
 from pprint import pprint
+import os
 import requests 
 r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=1fa58a5f84a8045a3e8405953748d0c0')
 pprint(r.json())
 
-
+import json
 import pyowm
 
-@app.route('/')
+@app.route('/' methods=['GET'])
 def index():
   return render_template('weather.html')
 
@@ -16,9 +17,12 @@ owm = pyowm.OWM('1fa58a5f84a8045a3e8405953748d0c0')
 observation =owm.weather_at_place('')
 w = observation.get_weather()
 
-while input= True
+def Get()
+
 print('The current temperature is ' + str(w.get_temperature()) +'and the wind blows at ' + str(w.get_wind()))
 
 if __name__=='__main__':
-	app.run(debug=True, host='0.0.0.0') 
+	port=int(os.environ.get("PORT",5000)
+	app.run(host='0.0.0.0', port=port)
+	app.run(debug=True) 
 	
